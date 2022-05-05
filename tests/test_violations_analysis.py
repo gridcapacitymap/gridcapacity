1fba88b18de30bbb4be4a4faa0fc53bbd602f851
Zagryzyn <vitek.khutronoy1@gmail.com>
2022-05-05 16:13:53 +0300

    Added return values

--- tests/test_violations_analysis.py
+++ tests/test_violations_analysis.py
@@ -10,19 +10,19 @@ class TestCheckViolations(unittest.TestCase):
     def setUpClass(cls) -> None:
         pssetools.init_psse()
 
-    def test_not_converged(self):
+    def test_not_converged(self) -> None:
         wf.open_case("savnw.sav")
         wf.branch_chng_3(154, 3008, realar1=10.0)
         self.assertEqual(Violations.NOT_CONVERGED, check_violations())
 
-    def test_bus_overvoltage(self):
+    def test_bus_overvoltage(self) -> None:
         wf.open_case("savnw.sav")
         self.assertEqual(
             Violations.BUS_OVERVOLTAGE,
             check_violations(max_trafo_loading_pct=110.0, max_bus_voltage_pu=0.9),
         )
 
-    def test_bus_undervoltage(self):
+    def test_bus_undervoltage(self) -> None:
         wf.open_case("iec60909_testnetwork_50Hz.sav")
         self.assertEqual(
             Violations.BUS_UNDERVOLTAGE,
@@ -31,7 +31,7 @@ class TestCheckViolations(unittest.TestCase):
             ),
         )
 
-    def test_branch_loading(self):
+    def test_branch_loading(self) -> None:
         wf.open_case("savnw.sav")
         wf.branch_chng_3(152, 3004, st=0)
         wf.branch_chng_3(153, 3006, st=0)
@@ -39,11 +39,11 @@ class TestCheckViolations(unittest.TestCase):
             Violations.BRANCH_LOADING, check_violations(max_trafo_loading_pct=115.0)
         )
 
-    def test_2w_trafo_loading(self):
+    def test_2w_trafo_loading(self) -> None:
         wf.open_case("savnw.sav")
         self.assertEqual(Violations.TRAFO_LOADING, check_violations())
 
-    def test_3w_trafo_loading(self):
+    def test_3w_trafo_loading(self) -> None:
         wf.open_case("iec60909_testnetwork_50Hz.sav")
         wf.three_wnd_imped_chng_4(1, 2, 8, "T3")
         self.assertEqual(
@@ -55,14 +55,14 @@ class TestCheckViolations(unittest.TestCase):
             ),
         )
 
-    def test_swing_bus_loading(self):
+    def test_swing_bus_loading(self) -> None:
         wf.open_case("savnw.sav")
         wf.load_data_6(3011, realar1=1000.0)
         self.assertEqual(
             Violations.SWING_BUS_LOADING, check_violations(max_trafo_loading_pct=110.0)
         )
 
-    def test_no_violations(self):
+    def test_no_violations(self) -> None:
         wf.open_case("savnw.sav")
         self.assertEqual(
             Violations.NO_VIOLATIONS, check_violations(max_trafo_loading_pct=110.0)
