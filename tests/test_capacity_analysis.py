901059f81cd5eb523f603b73b6f4e9d137905dd3
Zagryzyn <vitek.khutronoy1@gmail.com>
2022-05-17 16:07:26 +0300

    Add tests to check gen_avail_mva and load_avail_mva values Refactor usage of buses_headroom Extend ViolationsLimits to support new code

--- tests/test_capacity_analysis.py
+++ tests/test_capacity_analysis.py
@@ -17,6 +17,8 @@ import unittest
 
 import pssetools
 from pssetools.capacity_analysis import buses_headroom
+from pssetools.contingency_analysis import ContingencyScenario
+from pssetools.subsystems import Branch, Trafo
 from pssetools.violations_analysis import ViolationsLimits
 from tests import DEFAULT_CASE
 
@@ -25,32 +27,66 @@ class TestCheckCapacity(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         pssetools.init_psse()
+        cls.headroom = buses_headroom(
+            case_name=DEFAULT_CASE,
+            upper_load_limit_p_mw=100.0,
+            upper_gen_limit_p_mw=80.0,
+            normal_limits=ViolationsLimits(
+                max_bus_voltage_pu=1.05,
+                min_bus_voltage_pu=0.97,
+                max_branch_loading_pct=100.0,
+                max_trafo_loading_pct=110.0,
+                max_swing_bus_power_mva=1000.0,
+                branch_rate="Rate1",
+                trafo_rate="Rate1",
+            ),
+            contingency_scenario=ContingencyScenario(
+                branches=tuple(
+                    Branch(*args)
+                    for args in (
+                        [151, 201],
+                        [152, 202],
+                        [152, 3004],
+                        [153, 154],
+                        [153, 3006],
+                        [154, 203],
+                        [154, 3008],
+                    )
+                ),
+                trafos=(Trafo(3001, 3002), Trafo(3004, 3005)),
+            ),
+        )
 
-    normal_limits: ViolationsLimits = ViolationsLimits(
-        max_bus_voltage_pu=1.1,
-        min_bus_voltage_pu=0.9,
-        max_branch_loading_pct=120.0,
-        max_trafo_loading_pct=110.0,
-        max_swing_bus_power_mva=1000.0,
-    )
+    def test_load_average(self) -> None:
+        self.assertEqual(65.625 + 31.783638129984077j, self.headroom[3].load_avail_mva)
+
+    def test_load_high(self) -> None:
+        self.assertEqual(100 + 48.432210483785255j, self.headroom[0].load_avail_mva)
+
+    def test_load_zero(self) -> None:
+        self.assertEqual(0, self.headroom[5].load_avail_mva)
+
+    def test_gen_average(self) -> None:
+        self.assertEqual(25 + 12.108052620946314j, self.headroom[12].gen_avail_mva)
+
+    def test_gen_high(self) -> None:
+        self.assertEqual(52.5 + 25.42691050398726j, self.headroom[21].gen_avail_mva)
+
+    def test_gen_zero(self) -> None:
+        self.assertEqual(0, self.headroom[3].gen_avail_mva)
 
     def test_runtime_error(self) -> None:
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(
+            RuntimeError
+        ):  # expecting RuntimeError Violations.TRAFO_LOADING
             buses_headroom(
                 case_name=DEFAULT_CASE,
                 upper_load_limit_p_mw=100.0,
                 upper_gen_limit_p_mw=80.0,
             )
 
-    def test_violated_bus_amount(self) -> None:
+    def test_violated_bus_count(self) -> None:
         self.assertEqual(
             23,
-            len(
-                buses_headroom(
-                    case_name=DEFAULT_CASE,
-                    upper_load_limit_p_mw=100.0,
-                    upper_gen_limit_p_mw=80.0,
-                    normal_limits=self.normal_limits,
-                )
-            ),
+            len(self.headroom),
         )
