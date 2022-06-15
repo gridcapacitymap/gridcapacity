65656693fdb36e84942142ed320331f89125d8d5
Viktor Khutornoi <khutornoiv@gmail.com>
2022-06-15 18:39:37 +0300

    Move once-used variables to funcs., reduce Branch arguments

--- tests/test_contingency_analysis.py
+++ tests/test_contingency_analysis.py
@@ -16,42 +16,16 @@ from tests import DEFAULT_CASE
 
 
 class TestContingencyAnalysis(unittest.TestCase):
-    contingency_limits: ViolationsLimits
-    contingency_scenario: ContingencyScenario
-
     @classmethod
     def setUpClass(cls) -> None:
         pssetools.init_psse()
         wf.open_case(DEFAULT_CASE)
-        cls.contingency_limits = ViolationsLimits(
-            max_bus_voltage_pu=1.12,
-            min_bus_voltage_pu=0.88,
-            max_branch_loading_pct=120.0,
-            max_trafo_loading_pct=120.0,
-            max_swing_bus_power_mva=1000.0,
-            branch_rate="Rate2",
-            trafo_rate="Rate1",
-        )
-        cls.contingency_scenario = ContingencyScenario(
-            branches=(
-                Branch(from_number=151, to_number=152, branch_id="2 "),
-                Branch(from_number=151, to_number=201, branch_id="1 "),
-                Branch(from_number=152, to_number=202, branch_id="1 "),
-                Branch(from_number=152, to_number=3004, branch_id="1 "),
-                Branch(from_number=153, to_number=154, branch_id="1 "),
-                Branch(from_number=153, to_number=154, branch_id="2 "),
-                Branch(from_number=153, to_number=3006, branch_id="1 "),
-                Branch(from_number=154, to_number=203, branch_id="1 "),
-                Branch(from_number=154, to_number=3008, branch_id="1 "),
-            ),
-            trafos=(),
-        )
 
     def test_get_contingency_limiting_factor(self) -> None:
         self.assertEqual(
             LimitingFactor(
                 Violations.BRANCH_LOADING | Violations.BUS_UNDERVOLTAGE,
-                ss=Branch(from_number=154, to_number=205, branch_id="1"),
+                ss=Branch(154, 205),
             ),
             get_contingency_limiting_factor(
                 ContingencyScenario(branches=(Branch(154, 205),), trafos=()),
@@ -59,10 +33,34 @@ class TestContingencyAnalysis(unittest.TestCase):
         )
 
     def test_get_default_contingency_limits(self) -> None:
-        self.assertEqual(self.contingency_limits, get_default_contingency_limits())
+        self.assertEqual(
+            ViolationsLimits(
+                max_bus_voltage_pu=1.12,
+                min_bus_voltage_pu=0.88,
+                max_branch_loading_pct=120.0,
+                max_trafo_loading_pct=120.0,
+                max_swing_bus_power_mva=1000.0,
+                branch_rate="Rate2",
+                trafo_rate="Rate1",
+            ),
+            get_default_contingency_limits(),
+        )
 
     def test_get_contingency_scenario(self) -> None:
         self.assertEqual(
-            self.contingency_scenario,
+            ContingencyScenario(
+                branches=(  # "1 " with space is needed, the default value "1" without space is not working
+                    Branch(151, 152, "2 "),
+                    Branch(151, 201, "1 "),
+                    Branch(152, 202, "1 "),
+                    Branch(152, 3004, "1 "),
+                    Branch(153, 154, "1 "),
+                    Branch(153, 154, "2 "),
+                    Branch(153, 3006, "1 "),
+                    Branch(154, 203, "1 "),
+                    Branch(154, 3008, "1 "),
+                ),
+                trafos=(),
+            ),
             get_contingency_scenario(False, {"options1": 1, "options5": 1}),
         )
